35ec8d494a68d4b706b760b2283580745d0ad0d4
Vivian LEE <htleeab@gmail.com>
2020-04-05 22:11:46 +0800

    add google site verification for search console

--- source/conf.py
+++ source/conf.py
@@ -73,6 +73,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ['googleec3fcf9501d2264e.html']
 
 # -- CommonMark Markdown -----------------------------------------------------
 
